7fc33cf38581a08c3f7b4cb55c69467becc3f3eb
Albert Lyu <albertworld@gmail.com>
2015-01-02 16:16:19 -0600

    Fixed save_files.py to import stage module

--- save_files.py
+++ save_files.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from datetime import datetime, timedelta
 import sys
-import stage
+from stage import gdx
 
 if __name__ == "__main__":
   # Get list of dates and make sure dates are valid
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     sys.exit()
 
   # Fetch files given gid directories spanning date range
-  master_urls = stage.get_master_urls(dates)
+  master_urls = gdx.get_master_urls(dates)
   file_names = ['boxscore.json','linescore.json','game_events.json','plays.json']
   base_url = "http://gdx.mlb.com/components/game/"
   base_path = "data/"
   gid_dirs = []
   for url in master_urls:
-    gid_dirs = stage.get_gid_dirs(url)
+    gid_dirs = gdx.get_gid_dirs(url)
     game_file_urls = [gid_dir+file_name for gid_dir in gid_dirs for file_name in file_names]
 
     for game_file_url in game_file_urls:
-      stage.save_game_file(game_file_url,base_url,base_path)
+      gdx.save_game_file(game_file_url,base_url,base_path)
